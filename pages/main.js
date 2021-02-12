b234867f1317bd387c606405e697f3a20a6fa49a
alonso <alonsovillegas2016@gmail.com>
2021-02-12 16:52:23 -0500

    Style main image and GetStartedButton

--- pages/main.js
+++ pages/main.js
@@ -1,11 +1,49 @@
 import React from 'react'
 import Image from 'next/image'
+import { makeStyles, Button } from '@material-ui/core'
+
+const useStyles = makeStyles({
+    floatContainer: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        margin: 'auto',
+        zIndex: '10',
+    },
+    mainImage: {
+        color: 'white',
+        display: 'flex',
+        height: '100vh',
+        justifyContent: 'center',
+        width: '100%',
+        position:'relative'
+    },
+    getStartedButton: {
+        borderRadius: '4px',
+        background: 'rgb(0,198,162)',
+        fontSize: '16px',
+        height: 'max-content',
+        margin: '0 auto',
+        padding: '6px 16px',
+        width: 'max-content',
+        "&:hover": {
+            background: 'rgb(45,168,146)'
+        }
+    },
+    titleText: {
+        fontSize: '64px'
+    }
+});
 
 export default function Main() {
+    const classes = useStyles();
     return(
-    <main>
-         <h1>Welcome to Biv</h1>
-        <Image src="/mainImage.jpg" alt="main-image" width="550" height="450"/>
-    </main>
+         <div className={classes.mainImage}>
+             <div className={classes.floatContainer}>
+                <p className={classes.titleText}>Plan your trip</p>
+                <Button className={classes.getStartedButton} color="inherit">Get Started!</Button>
+             </div>
+            <Image src="/mainImage.jpg" alt="main-image" layout='fill' />
+         </div>
     );
 }
