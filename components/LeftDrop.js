797fe404aa453b4da48a363a97aea98c9feac70b
alonso <alonsovillegas2016@gmail.com>
2021-05-26 02:54:41 -0500

    Add some styles to LeftDrop

--- components/LeftDrop.js
+++ components/LeftDrop.js
@@ -1,26 +1,69 @@
-import { makeStyles } from "@material-ui/core"
+import { Button, makeStyles } from "@material-ui/core"
 import CommentForm from "./CommentForm"
 import SearchField from "../components/SearchField"
-import ResourceBox from "./ResourceBox";
+import ResourceBox from "./ResourceBox"
+import Icon from '@mdi/react'
+import { mdiChevronLeft, mdiChevronRight } from '@mdi/js'
 
 const useStyles = makeStyles({
-    root: {
-        background: 'grey',
+    dropContainer:{
+        display: 'grid',
+        gridTemplateColumns: '420px 0.5rem',
+        height: '100%'
+    },
+    dropRoot: {
+        background: 'white',
         width: '430px',
         height: '100%',
+        zIndex: 2,
+        minWidth: '430px',
+        display: 'flex',
+        flexDirection: 'column',
+        border: '1px grey',
+        borderStyle: 'none solid',
+    },
+    closeDropButton: { 
         zIndex: 1,
-        minWidth: '430px'
+        position: 'relative', 
+        alignSelf: 'center',
+        background: 'white',
+        width: '40px',
+        height: '70px',
+        borderRadius: '0 100px 100px 0',
+        border: '1px solid grey',
+        minWidth: '0px'
+    },
+    dropBoxContainer: {
+        width: '100%',
+        height: '100%',
+        background: 'white',
+        display: 'flex',
+        flexDirection: 'column',
+        position: 'relative', 
+        alignSelf: 'center',
+        margin: '2rem 0 0'
     }
 });
 
 export default function LeftDrop() {
     const classes = useStyles();
-    return <div className={classes.root}>
-        <SearchField/>
-        <CommentForm formType='comment'/>
-        <CommentForm formType='description'/>
-        <ResourceBox resourcesType='Images' resources={['image', 'image', 'image']}/>
-        <ResourceBox resourcesType='Videos' resources={['video', 'video', 'video']}/>
-        <ResourceBox resourcesType='Recent' resources={['recent', 'recent', 'recent']}/>
-    </div>
+    return <div className={classes.dropContainer}>
+                <div className={classes.dropRoot}>
+                    {/* <CommentForm formType='comment'/>
+                    <CommentForm formType='description'/> */}
+                    <div className={classes.dropBoxContainer}>
+                        <SearchField/>
+                        <ResourceBox resourcesType='Images' resources={['image', 'image', 'image']}/>
+                        <ResourceBox resourcesType='Videos' resources={['video', 'video', 'video']}/>
+                        <ResourceBox resourcesType='Recent' resources={['recent', 'recent', 'recent']}/>
+                    </div>
+                </div>
+                <Button className={classes.closeDropButton}>
+                    <Icon
+                        name="Close Drop"
+                        path={mdiChevronLeft}
+                        size={1}
+                    />
+                </Button>
+        </div> 
 }
--- components/ResourceBox.js
+++ components/ResourceBox.js
@@ -1,37 +1,80 @@
-import { Button, Typography } from "@material-ui/core"
+import { Button, makeStyles, Typography } from "@material-ui/core"
 import Icon from '@mdi/react'
 import { mdiChevronDown, mdiChevronLeft, mdiChevronRight } from '@mdi/js'
+import { useState } from "react";
+
+const useStyles = makeStyles({
+    title: {
+        display: 'flex',
+        flexDirection:'row',
+        cursor: 'pointer',
+        margin: '1rem',
+        width: 'fit-content'
+    },
+    boxRoot: {
+        margin: 10
+    },
+    titleText: {
+        marginRight: '6px'
+    },
+    slideButton: {
+        minWidth: '0px',
+        padding: '0',
+        width: '28px',
+        height: '28px'
+    },
+    resourceElement: {
+        background: 'grey',
+        width: '100px',
+        height: '100%',
+        margin: '0 5px'
+    },
+    resourceDrop: {
+        height: '100px',
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent:'center'
+    }
+});
 
 export default function ResourceBox(props) {
     const { resourcesType, resources } = props;
-    return <div style={{margin: 10}}>
-        <div style={{ display: 'flex', flexDirection:'row', cursor: 'pointer' }}>
-            <Typography>
+    const classes = useStyles();
+
+    const chargeElements = (resources) => {
+        let elements = [];
+        if ( resources ) {
+            for (let i=0; i<3; i++) {
+                elements.push(<div className={classes.resourceElement}>
+                                {resourcesType}
+                            </div>)}
+        }
+        return elements;
+    }
+
+    const [state, setState] = useState({ showing: chargeElements(resources)});
+
+    return <div className={classes.boxRoot}>
+        <div className={classes.title}>
+            <Typography className={classes.titleText} variant='subtitle1'>
                 {resourcesType}
             </Typography>
             <Icon path={mdiChevronDown}
                 title="Expand List"
                 size={1}
-                />
+            />
         </div>
-        <div
-            className='ResourceDrop' 
-            style={{height: 100, display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent:'center'}}
-            >
-            <Button>
+        <div className={classes.resourceDrop}>
+            <Button className={classes.slideButton}>
                 <Icon
-                path={mdiChevronLeft}
-                title="Prev Element"
-                size={1}
+                    path={mdiChevronLeft}
+                    title="Prev Element"
+                    size={1}
                 />
             </Button>
-            {resources.map( element =>  <div 
-                                            className='ResourceElement'
-                                            style={{background: 'white', width: 100, height: '100%', margin: '0 5px'}}
-                                            >
-                                            {resourcesType}
-                                        </div>)}
-            <Button>
+            {state.showing.map((element)=>element)}
+            <Button className={classes.slideButton}>
                 <Icon
                     path={mdiChevronRight}
                     title="Next Element"
